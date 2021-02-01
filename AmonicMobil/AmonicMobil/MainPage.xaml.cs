9c3ef2c49734c00ad49b23ff0821a501ac4bef51
VoX125ZeD <VoX125ZeD@DESKTOP-DQAE9IS>
2021-02-01 18:53:14 +0500

    Добавьте файлы проекта.

---
+++ AmonicMobil/AmonicMobil/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AmonicMobil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            img1.Source =ImageSource.FromResource("AmonicMobil.img.png");
+            
+
+
+        }
+        
+        private async void  but1_Clicked(object sender, EventArgs e)
+        {
+            Page os = new Page1();
+            
+            await Navigation.PushAsync(os);
+        }
+    }
+}
---
+++ AmonicMobil/AmonicMobil/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AmonicMobil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            img1.Source = ImageSource.FromResource("AmonicMobil.img1.png");
+            
+        
+        }
+    }
+}
